24b0b7d0bf5bc033acff3741673649727269490e
madi308 <77383233+madi308@users.noreply.github.com>
2025-01-16 20:28:52 +0200

    Escape checks if there are any menus open and opens the main menu if there are no menus open.

--- Assets/Scripts/Controllers/MenuController.cs
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -7,13 +7,23 @@ using UnityEngine.SceneManagement;
 public class MenuController : MonoBehaviour
 {
     public string mainMenuSceneName;
+    public GameObject businessBuilder;
+    public GameObject chooseBuildings;
+    public GameObject levelUpScreen;
     
     private void Update()
     {
-        if (Input.GetButton("ExitMenu"))
+        if (Input.GetButton("ExitMenu") && !IsMenuOpen())
         {
-            //SaveSystem.Save();
-            //SceneManager.LoadScene(mainMenuSceneName);
+            SaveSystem.Save();
+            SceneManager.LoadScene(mainMenuSceneName);
         }
+    }
+
+    private bool IsMenuOpen()
+    {
+        return businessBuilder.activeSelf
+               || chooseBuildings.activeSelf
+               || levelUpScreen.activeSelf;
     }
 }

a80a4a97253aae10b89df00f0529d7cbb6373b41
madi308 <77383233+madi308@users.noreply.github.com>
2025-01-16 18:45:49 +0200

    Esc opens main menu

---
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public string mainMenuSceneName;
+    
+    private void Update()
+    {
+        if (Input.GetButton("ExitMenu"))
+        {
+            SaveSystem.Save();
+            SceneManager.LoadScene(mainMenuSceneName);
+        }
+    }
+}
